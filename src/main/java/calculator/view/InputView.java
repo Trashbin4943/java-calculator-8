3b958d23e447ea25289f13af8c1a23b4799b6dce
Trashbin4943 <anthonyhwan415@gmail.com>
2025-10-20 17:14:24 +0900

    Revert "REFACTOR: 외부 출력 텍스트 수정"
    This reverts commit f0c83106cb2b72ba4f0dc199f0ca242375049522.

--- src/main/java/calculator/view/InputView.java
+++ src/main/java/calculator/view/InputView.java
@@ -4,7 +4,7 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
 
     public static String getInputString() {
-        System.out.println("덧셈할 문자열을 입력하세요.");
+        System.out.println("덧셈할 문자열을 입력하세요:");
         return Console.readLine();
     }
 }
--- src/main/java/calculator/view/OutputView.java
+++ src/main/java/calculator/view/OutputView.java
@@ -2,6 +2,6 @@ package calculator.view;
 
 public class OutputView {
     public static void printResult(int sum) {
-        System.out.print("결과 : " + sum);
+        System.out.print("계산 결과: " + sum);
     }
 }
